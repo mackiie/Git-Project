8a28b6d9ab9884fbba3a3e1222db1941c242bfd8
Neha <1300nehasingh@gmail.com>
2019-04-24 15:53:38 +0530

    1 commit by Neha

--- file.cpp
+++ file.cpp
@@ -102,3 +102,85 @@ cout<<"\nType Comfort (S/N) "<<tempRoom.type;
 cout<<"\nType Size (B/S) "<<tempRoom.stype;
 cout<<"\nRent: "<<tempRoom.rent;
 }
+
+
+/////////////////////////////////////////////////editted by Neha
+
+//Global Declarations
+class Room rooms[max];
+int count=0;
+
+
+Room Room::addRoom(int rno)
+{
+class Room room;
+room.roomNumber=rno;
+cout<<"\nType AC/Non-AC (A/N) : ";
+cin>>room.ac;
+cout<<"\nType Comfort (S/N) : ";
+cin>>room.type;
+cout<<"\nType Size (B/S) : ";
+cin>>room.stype;
+cout<<"\nDaily Rent : ";
+cin>>room.rent;
+room.status=0;
+
+cout<<"\n Room Added Successfully!";
+getch();
+return room;
+}
+
+
+void Room::searchRoom(int rno)
+{
+int i,found=0;
+for(i=0;i<count;i++)
+{
+if(rooms[i].roomNumber==rno)
+{
+found=1;
+break;
+}
+}
+if(found==1)
+{
+cout<<"Room Details\n";
+if(rooms[i].status==1)
+{
+cout<<"\nRoom is Reserved";
+}
+else
+{
+cout<<"\nRoom is available";
+}
+displayRoom(rooms[i]);
+getch();
+}
+else
+{
+cout<<"\nRoom not found";
+getch();
+}
+}
+
+void Room::displayRoom(Room tempRoom)
+{
+cout<<"\nRoom Number: \t"<<tempRoom.roomNumber;
+cout<<"\nType AC/Non-AC (A/N) "<<tempRoom.ac;
+cout<<"\nType Comfort (S/N) "<<tempRoom.type;
+cout<<"\nType Size (B/S) "<<tempRoom.stype;
+cout<<"\nRent: "<<tempRoom.rent;
+}
+
+//hotel management class
+class HotelMgnt:protected Room
+{
+public:
+void checkIn();
+void getAvailRoom();
+void searchCustomer(char *);
+void checkOut(int);
+void guestSummaryReport();
+};
+
+//////Editting done
