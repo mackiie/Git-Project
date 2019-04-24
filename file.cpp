fd53293504f032491bc68223bf3340805861f6dd
prashasti0312 <500068493@stu.upes.ac.in>
2019-04-24 22:34:22 +0530

    second commit by prashasti

--- file.cpp
+++ file.cpp
@@ -292,4 +292,138 @@ class Queue
 
     }
 
-  
+  void pop()
+
+    {
+
+      if(front==rear)
+
+      {
+
+        cout<<"Queue Underflow";
+
+      }
+
+      else
+
+      {
+
+        front++;
+
+      }
+
+    }
+
+    void display()
+
+    {
+
+      if (front == rear)
+
+      {
+
+        cout<<"Queue is empty";
+
+      }
+
+      else
+
+      {
+
+        cout<<"Queue is : ";
+
+        for(i=front;i<rear;i++)
+
+        {
+
+          cout<<array[i]<<" ";
+
+        }
+
+      }
+
+    }
+
+};
+
+int main()
+
+{
+
+  int choice;
+
+  Queue q;
+
+  while(1)
+
+  cout<<"--------------------------------QUEUE USING ARRAY-------------------------------";
+
+    cout<<endl<<endl<<endl<<endl;
+
+    cout<<endl<<"1.\tPush elemnt in Stack";
+
+    cout<<endl<<"2.\tPop elemnt from Stack";
+
+    cout<<endl<<"3.\tDisplay elemnts in Stack";
+
+    cout<<endl<<endl<<"0.\tExit";
+
+    cout<<endl<<endl<<endl;
+
+    cout<<"Enter your choice : ";
+
+    cin>>choice;
+
+    cout<<endl<<endl;
+
+    switch(choice)
+
+    {
+
+      case 1:
+
+        q.push();
+
+        cout<<endl<<endl<<endl;
+
+        break;
+
+      case 2:
+
+        q.pop();
+
+        cout<<endl<<endl<<endl;
+
+        break;
+
+      case 3:
+
+        q.display();
+
+        cout<<endl<<endl<<endl;
+
+        break;
+
+      case 0:
+
+        exit(0);
+
+        cout<<endl<<endl<<endl;
+
+        break;
+
+      default:
+
+        cout<<"Wrong Choice";
+
+        cout<<endl<<endl<<endl;
+
+        break;
+
+    }
+
+  }
+
+  return 0;
+
+} 
