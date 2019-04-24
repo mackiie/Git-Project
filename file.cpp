55239b961e4bdb4f8e1823e5453cdb10c9a19dc5
prashasti0312 <500068493@stu.upes.ac.in>
2019-04-24 23:03:45 +0530

    first commit by prashasti

--- file.cpp
+++ file.cpp
@@ -13,7 +13,55 @@ public:
 char name[100];
 char address[100];
 char phone[12];
-char from_date[20];
+void setComplex(void)
+
+{
+
+cout << "Enter the real and imaginary parts : ";
+
+cin >> this->real;
+
+cin >> this->imag;
+
+}
+
+Complex add(const Complex& c)
+
+{
+
+Complex comp;
+
+comp.real = this->real + c.real;
+
+comp.imag = this->imag + c.imag;
+
+return comp;
+
+}
+
+Complex subtract(const Complex& c)
+
+{
+
+Complex comp;
+
+comp.real = this->real - c.real;
+
+comp.imag = this->imag - c.imag;
+
+return comp;
+
+}
+
+void printComplex(void)
+
+{
+
+cout << "Real : " << this->real << endl<< "Imaginary : " << this->imag << endl;
+
+}
+
+};char from_date[20];
 char to_date[20];
 float payment_advance;
 int booking_id;
