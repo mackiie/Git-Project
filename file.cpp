8e67cbc5e664063fb5b33d3a32bc78006e6a3be7
prashasti0312 <500068493@stu.upes.ac.in>
2019-04-24 15:56:39 +0530

    second commit

--- file.cpp
+++ file.cpp
@@ -117,5 +117,80 @@ float payment_advance;
 int booking_id;
 };
 
+void HotelMgnt::guestSummaryReport(){
+
+if(count==0){
+	cout<<"\n No Guest in Hotel !!";
+}	
+for(int i=0;i<count;i++)
+{
+if(rooms[i].status==1)
+{
+cout<<"\n Customer First Name : "<<rooms[i].cust.name;
+cout<<"\n Room Number : "<<rooms[i].roomNumber;
+cout<<"\n Address (only city) : "<<rooms[i].cust.address;
+cout<<"\n Phone : "<<rooms[i].cust.phone;
+cout<<"\n---------------------------------------";	
+}
+	
+}
+
+getch();
+}
+
+//hotel management reservation of room
+void HotelMgnt::checkIn()
+{
+int i,found=0,rno;
+
+class Room room;
+cout<<"\nEnter Room number : ";
+cin>>rno;
+for(i=0;i<count;i++)
+{
+if(rooms[i].roomNumber==rno)
+{
+found=1;
+break;
+}
+}
+if(found==1)
+{
+if(rooms[i].status==1)
+{
+cout<<"\nRoom is already Booked";
+getch();
+return;
+}
+
+cout<<"\nEnter booking id: ";
+cin>>rooms[i].cust.booking_id;
+
+cout<<"\nEnter Customer Name (First Name): ";
+cin>>rooms[i].cust.name;
+
+cout<<"\nEnter Address (only city): ";
+cin>>rooms[i].cust.address;
+
+cout<<"\nEnter Phone: ";
+cin>>rooms[i].cust.phone;
+
+cout<<"\nEnter From Date: ";
+cin>>rooms[i].cust.from_date;
+
+cout<<"\nEnter to  Date: ";
+cin>>rooms[i].cust.to_date;
+
+
+cout<<"\nEnter Advance Payment: ";
+cin>>rooms[i].cust.payment_advance;
+
+rooms[i].status=1;
+
+cout<<"\n Customer Checked-in Successfully..";
+getch();
+}
+}
+
 
 
