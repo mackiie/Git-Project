c972b124870c3cb1c306239c4728600e846a3c17
prashasti0312 <500068493@stu.upes.ac.in>
2019-04-24 16:07:04 +0530

    second commit by prashasti

--- file.cpp
+++ file.cpp
@@ -252,5 +252,53 @@ break;
 
 getch();
 }
-
+switch(opt)
+{
+case 1:
+manageRooms();
+break;
+case 2:
+if(count==0)
+{
+cout<<"\nRooms data is not available.\nPlease add the rooms first.";
+getch();
+}
+else
+hm.checkIn();
+break;
+case 3:
+if(count==0)
+{
+cout<<"\nRooms data is not available.\nPlease add the rooms first.";
+getch();
+}
+else
+hm.getAvailRoom();
+break;
+case 4:
+if(count==0)
+{
+cout<<"\nRooms are not available.\nPlease add the rooms first.";
+getch();
+}
+else
+{
+cout<<"Enter Customer Name: ";
+cin>>pname;
+hm.searchCustomer(pname);
+}
+break;
+case 5:
+if(count==0)
+{
+cout<<"\nRooms are not available.\nPlease add the rooms first.";
+getch();
+}
+else
+{
+cout<<"Enter Room Number : ";
+cin>>rno;
+hm.checkOut(rno);
+}
+break;
 
