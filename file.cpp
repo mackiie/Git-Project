0a68d53d4987a9da182ec43b638d2c2e44e847b7
KHUSHBU KATHURIA <khushbuk2k@gmail.com>
2019-04-25 21:03:34 +0530

    khushbu 7th commit

--- file.cpp
+++ file.cpp
@@ -695,4 +695,98 @@ class Queue
 
 };
 
+class Binary
+
+{
+
+  private:
+
+                              int num;
+
+                              int arr[50];
+
+                              int i;
+
+  public:
+
+                              void input()
+
+                              {
+
+                                             cout<<"Enter number to convert to binary (Must be greater than 0) : ";
+
+                                             cin>>num;
+
+                                             if(num<0)
+
+                                             {
+
+                                                            input();
+
+                                             }
+
+      else if(num==0)
+
+      {
+
+        i=0;
+
+        arr[i]=0;
+
+        i++;
+
+      }
+
+      else
+
+      {
+
+        convert();
+
+      }
+
+                              }
+
+                              void convert()
+
+                              {
+
+                                             i=0;
+
+                                             while(num>0)
+
+                                             {
+
+                                                            arr[i]=num%2;
+
+                                                            num=num/2;
+
+                                                            i++;
+
+                                             }
+
+                              }
+
+                              void display()
+
+                              {
+
+                                             cout<<"Binary No : ";
+
+                                             i--;
+
+                              while(i>=0)
+
+                                             {
+
+                                                            cout<<arr[i];
+
+                                                            i--;
+
+                                             }
+
+                              }
+
+};
+
 
